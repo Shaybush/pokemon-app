7a2d08679a6b017b4145a42061160aa870ecb5b8
Shay Bushary <shaybush93@gmail.com>
2024-09-19 17:49:27 +0300

    add basic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import './App.css'
+
 function App() {
-  
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=50";
+
   return (
     <>
+    Pokemon app
     </>
   )
 }
